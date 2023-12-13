986cc942598ec5874eb48d2eeb83adcbc38f79b0
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-13 14:29:21 -0300

    m

--- curso1/jogoteca.py
+++ curso1/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -13,6 +13,7 @@ jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [ jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = "Alura"
 
 @app.route('/')
 def index():
@@ -30,6 +31,29 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
 
+    return redirect('/')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(f'{session["usuario_logado"]} logado com sucesso')
+        return redirect('/')
+    else:
+        flash('Usuário não logado')
+        return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetudao com sucesso!')
     return redirect('/')
 
 app.run(debug=True)
